c56b8fac1cb5932c20d429f008e2d7068ffb2a50
liuweiqiang <liuweiqiang2014@xiaochuankeji.cn>
2021-04-13 16:26:18 +0800

    adjust log

--- controllers/autotest/user_list.go
+++ controllers/autotest/user_list.go
@@ -14,8 +14,9 @@ type UserList struct {
 // 获取用户列表 登录
 func (c *AutoTestController) userList() {
 	ul := UserList{}
+	l := logs.GetBeeLogger()
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &ul); err != nil {
-		logs.Error(1024, err)
+		l.Error("bad param")
 		c.ErrorJson(-1, "请求参数错误", nil)
 	}
 	if ul.Page <= 0 || ul.Offset <= 0 {
--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	_ "go_autoapi/routers"
 )
 
-func main() {
+func init() {
+	_ = logs.SetLogger(logs.AdapterFile, `{"filename":"/Users/xiaoqiang/data/log/project.log","level":7,"maxlines":0,"maxsize":0,"daily":true,"maxdays":10,"color":true}`)
 	logs.EnableFuncCallDepth(true)
-	logs.Info("debug")
+	logs.SetLogFuncCallDepth(3)
+}
+
+func main() {
 	beego.Run()
 }
