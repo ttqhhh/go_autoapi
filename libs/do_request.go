fd8f2c67357c55f79e2e71fa96f0303eea179b93
liuweiqiang <liuweiqiang2014@xiaochuankeji.cn>
2021-04-22 21:08:29 +0800

    update ajson-->jsonpath

--- libs/do_request.go
+++ libs/do_request.go
@@ -6,8 +6,9 @@ import (
 	"fmt"
 	"github.com/astaxie/beego/logs"
 	"github.com/bitly/go-simplejson"
-	"github.com/spyzhov/ajson"
+	jsonpath "github.com/spyzhov/ajson"
 	"go_autoapi/db_proxy"
+	"go_autoapi/models"
 	"io/ioutil"
 	"net/http"
 	"reflect"
@@ -51,7 +52,7 @@ func HttpPost(postUrl string, headers map[string]string, jsonMap map[string]inte
 	return resp.StatusCode, string(body), cookieStr
 }
 
-func DoRequest(url string, uuid string, data map[string]interface{}, verify map[string]map[string]interface{}) {
+func DoRequest(url string, uuid string, data map[string]interface{}, verify map[string]map[string]interface{}, caseId int64) {
 	//密码
 	r := db_proxy.GetRedisObject()
 	statusCode, body, _ := HttpPost(url, nil, data)
@@ -72,94 +73,25 @@ func DoRequest(url string, uuid string, data map[string]interface{}, verify map[
 	// 判断某个字段的类型
 	//fmt.Println("type:", reflect.TypeOf(jmap["code"]))
 	//判断登录是否成功
-	doVerifyV2(statusCode, uuid, body, verify)
+	doVerifyV2(statusCode, uuid, body, verify, caseId)
 	r.Incr(uuid)
 
 }
 
-// 增加验证函数，比较响应和需要验证的内容
-func doVerify(statusCode int, uuid string, response string, verify map[string]map[string]interface{}) {
-	var jmap map[string]interface{}
-	if err := json.Unmarshal([]byte(response), &jmap); err != nil {
-		fmt.Println("解析失败", err)
-		return
-	}
-	root, _ := ajson.JSONPath([]byte(response), "$.data.id")
-	fmt.Println("root is ", root)
-	data := jmap["data"].(map[string]interface{})
-	if statusCode != 200 {
-		fmt.Println("请求返回状态不是200，请求失败")
-		return
-	}
-	for k, v := range verify {
-		//fmt.Println(k, v, verify, reflect.TypeOf(verify))
-		logs.Error("k,v is ", k, v, reflect.TypeOf(k))
-		if k != "code" && data[k] == nil {
-			logs.Error("the verify key is not exist in the response", k)
-			return
-		}
-		for subK, subV := range v {
-			if subK == "eq" {
-				if k == "code" {
-					logs.Error("code here ,OK no problem", jmap["code"], subV)
-					if jmap["code"] != subV {
-						logs.Error("code not equal", jmap["code"], subV, subK)
-						return
-					}
-				} else if data[k] != subV {
-					logs.Error("not equal", data[k], subV)
-					return
-				}
-			} else if subK == "lt" {
-				if subV.(float64) >= data[k].(float64) {
-					logs.Error("not lt", data[k], subV)
-					return
-				}
-			} else if subK == "gt" {
-				if subV.(int64) <= data[k].(int64) {
-					logs.Error("not gt", data[k], subV)
-					return
-				}
-			} else if subK == "lte" {
-				if subV.(int64) > data[k].(int64) {
-					logs.Error("not lte", data[k], subV)
-					return
-				}
-			} else if subK == "gte" {
-				if subV.(int64) < data[k].(int64) {
-					logs.Error("not gte", data[k], subV)
-					return
-				}
-			} else if subK == "need" {
-				logs.Error("need string", data[k], subV)
-				if data[k] == nil {
-					logs.Error("not need", data[k], subV)
-					return
-				}
-			} else if subK == "in" {
-				b := strings.ContainsAny(data[k].(string), subV.(string))
-				if b == false {
-					logs.Error("not in", data[k], subV)
-					return
-				}
-			} else {
-				logs.Error("do not support")
-				return
-			}
-		}
-	}
-}
-
 // 采用jsonpath 对结果进行验证
-func doVerifyV2(statusCode int, uuid string, response string, verify map[string]map[string]interface{}) {
+func doVerifyV2(statusCode int, uuid string, response string, verify map[string]map[string]interface{}, caseId int64) {
 
 	if statusCode != 200 {
-		fmt.Println("请求返回状态不是200，请求失败")
+		logs.Error("请求返回状态不是200，请求失败")
+		err := models.InsertResult(uuid, caseId, "请求返回状态不是200")
+		if err != nil {
+			logs.Error("verify status code dump the result error", err)
+		}
 		return
 	}
 	// 提前检查jsonpath是否存在，不存在就报错
 	for k := range verify {
-		verifyO, err := ajson.JSONPath([]byte(response), k)
+		verifyO, err := jsonpath.JSONPath([]byte(response), k)
 		if err != nil {
 			logs.Error("doVerifyV2 jsonpath error，test failed", err)
 		}
@@ -171,7 +103,7 @@ func doVerifyV2(statusCode int, uuid string, response string, verify map[string]
 	for k, v := range verify {
 		//fmt.Println(k, v, verify, reflect.TypeOf(verify))
 		logs.Error("k,v is ", k, v, reflect.TypeOf(k))
-		verifyO, _ := ajson.JSONPath([]byte(response), k)
+		verifyO, _ := jsonpath.JSONPath([]byte(response), k)
 		for subK, subV := range v {
 			var vv interface{}
 			// 根据类型转换jsonpath获取的数组首位类型
