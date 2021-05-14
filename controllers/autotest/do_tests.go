8cc0b8a56c755d6d401583f6afada1ba7b978f10
xueyibing <xueyibing2014@xiaochuankeji.cn>
2021-05-14 15:02:24 +0800

    线上巡检临时提交

--- controllers/autotest/do_tests.go
+++ controllers/autotest/do_tests.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego/logs"
-	uuid "github.com/satori/go.uuid"
 	constant "go_autoapi/constants"
 	"go_autoapi/libs"
 	"go_autoapi/models"
@@ -189,84 +188,4 @@ func (c *AutoTestController) performTests() {
 		}()
 	}()
 	c.SuccessJsonWithMsg(map[string]interface{}{"uuid": uuid, "count": count}, "OK")
-}
-
-// 执行线上巡检Case
-func PerformInspection(businessId int8, serviceId int64) (err error) {
-	userId := "系统定时任务"
-	u2 := uuid.NewV4()
-	uuid := u2.String()
-
-	mongo := models.TestCaseMongo{}
-	// 根据不同的执行维度，聚合需要执行的所有Case集合
-	caseList := []*models.TestCaseMongo{}
-
-	// 查询指定服务下所有的Case
-	caseList, err = mongo.GetAllInspectionCasesByService(serviceId)
-	if err != nil {
-		logs.Error("获取测试用例列表失败, err: ", err)
-		return
-	}
-	if len(caseList) == 0 {
-		logs.Info("当前服务没有线上巡检用例, serviceId: ", serviceId)
-		return
-	}
-	// 对本次执行操作记录进行保存
-	totalCases := len(caseList)
-	runReport := models.RunReportMongo{}
-	// 报告的名字：业务线-执行人-时间戳（日期）
-	businessMap := GetAllBusinesses()
-	businessName := "未知"
-	for _, v := range businessMap {
-		if int8(v["code"].(int)) == businessId {
-			businessName = v["name"].(string)
-			break
-		}
-	}
-	format := "20060102/150405"
-	runReport.Name = businessName + "-" + userId + "-" + time.Now().Format(format)
-	runReport.CreateBy = userId
-	runReport.RunId = uuid
-	runReport.TotalCases = totalCases
-	runReport.IsPass = models.RUNNING
-	runReport.Business = businessId
-	runReport.ServiceName = caseList[0].ServiceName
-
-	id, err := runReport.Insert(runReport)
-	if err != nil {
-		logs.Error("线上巡检时， 插入执行记录失败", err)
-		return
-	}
-
-	go func() {
-		wg := sync.WaitGroup{}
-		wg.Add(len(caseList))
-		for _, val := range caseList {
-			go func(url string, uuid string, param string, checkout string, caseId int64, runBy string) {
-				libs.DoRequestV2(url, uuid, param, checkout, caseId, runBy)
-				// 获取用例执行进度时使用
-				r := utils.GetRedis()
-				r.Incr(constant.RUN_RECORD_CASE_DONE_NUM + uuid)
-				wg.Done()
-			}(val.ApiUrl, uuid, val.Parameter, val.Checkpoint, val.Id, userId)
-		}
-		wg.Wait()
-
-		go func() {
-			autoResult, _ := models.GetResultByRunId(uuid)
-			var isPass int8 = models.SUCCESS
-			// 判断case执行结果集合中是否有失败的case，有则认为本次执行操作状态为FAIL
-			for _, result := range autoResult {
-				if result.Result == models.AUTO_RESULT_FAIL {
-					isPass = models.FAIL
-					break
-				}
-			}
-			// 更新失败个数和本次执行记录状态
-			autoResultMongo := &models.AutoResult{}
-			failCount, _ := autoResultMongo.GetFailCount(uuid)
-			runReport.UpdateIsPass(id, isPass, failCount, userId)
-		}()
-	}()
-	return
 }
--- task/task.go
+++ task/task.go
@@ -3,38 +3,16 @@ package task
 import (
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/toolbox"
-	controllers "go_autoapi/controllers/autotest"
-	"go_autoapi/models"
 )
 
 func init() {
-	// new一个定时任务, 每天早上9点执行
-	tk := toolbox.NewTask("Inspection_Task", "0 0 9 * * ? *",
-		func() error {
-			logs.Info("启动定时任务：Product Inspection")
-			// 获取所以业务线，并进行遍历所有的业务线
-			businesses := controllers.GetAllBusinesses()
-			for _, business := range businesses {
-				// 遍历业务线下的所有所有服务
-				serviceMongo := models.ServiceMongo{}
-				businessId := int8(business["code"].(int))
-				serviceMongos, err := serviceMongo.QueryByBusiness(businessId)
-				if err != nil {
-					logs.Error("执行线上巡检定时任务时，查询指定业务线下的服务时报错， err: ", err)
-					return err
-				}
-				// 遍历服务下边所有的巡检Case
-				for _, service := range serviceMongos {
-					controllers.PerformInspection(businessId, service.Id)
-				}
-			}
-			return nil
-		})
-	err := tk.Run()
+	// new一个线上巡检的定时任务定时任务
+	onlineInspectionTask := toolbox.NewTask("Inspection_Task", ONLINE_INSPECTION_EXPRESSION, OnlineInspection)
+	err := onlineInspectionTask.Run()
 	if err != nil {
 		logs.Error("运行系统定时任务报错， err: ", err)
 		return
 	}
-	toolbox.AddTask("Inspection_Task", tk)
+	toolbox.AddTask("Inspection_Task", onlineInspectionTask)
 	toolbox.StartTask()
 }
