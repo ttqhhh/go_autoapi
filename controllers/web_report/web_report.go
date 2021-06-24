4cda88b95092a2a662d817a17da1f2794cc0d6b8
sunzhiying <sunzhiying2014@xiaochuankeji.cn>
2021-06-24 14:38:49 +0800

    1、查看邮箱组时弹窗变小 2、邮箱格式校验，后缀、分隔符、中文 3、添加邮箱时，校验是否包含自己 4、下拉框放到收件人上边展示 5、创建邮箱组增加必填项 6、未选择邮箱组时，不能发送邮寄

--- controllers/web_report/web_report.go
+++ controllers/web_report/web_report.go
@@ -68,7 +68,7 @@ func (c *WebreportController) Post() {
 	case "query":
 		c.AllWebReport()
 	case "insert_email":
-		c.InsertEmail()
+		c.SaveEmail()
 	case "queryEmail":
 		c.QueryEmail()
 
@@ -95,7 +95,7 @@ func (c *WebreportController) ShowEmail() {
 }
 
 //添加邮箱组
-func (c *WebreportController) InsertEmail() {
+func (c *WebreportController) SaveEmail() {
 	userId, _ := c.GetSecureCookie(constant.CookieSecretKey, "user_id")
 	email := &models.EmailMongo{}
 	err := c.ParseForm(email)
@@ -126,6 +126,26 @@ func (c *WebreportController) getById() {
 	id, err := c.GetInt64("id")
 	if err != nil {
 		logs.Warn("/service/getById接口 参数异常, err: %v", err)
+		c.ErrorJson(-1, "参数异常", nil)
+	}
+	logs.Info("请求参数: id=%v", id)
+	emailMongo := models.EmailMongo{}
+	email, err := emailMongo.QueryById(id)
+	if err != nil {
+		c.ErrorJson(-1, "服务查询数据异常", nil)
+	}
+	c.Data["data"] = email
+	//c.Data["a"] = &email
+	c.TplName = "web_email_detail.html"
+
+	//c.SuccessJson(email)
+}
+
+//编辑邮件组
+func (c *WebreportController) edit() {
+	id, err := c.GetInt64("id")
+	if err != nil {
+
 		c.ErrorJson(-1, "参数异常", nil)
 	}
 	logs.Info("请求参数: id=%v", id)
@@ -246,39 +266,112 @@ func insert(name, desc, xmyl, jszb, fx, sm, zb, recipient string) error {
 	return nil
 }
 
-//添加测试报告
-func (c *WebreportController) Insert() {
+//遍历切片是否包含某个值
+func in(target string, str_array []string) bool {
+
+	for _, element := range str_array {
+
+		if target == element {
+
+			return true
+		}
+	}
+	return false
+}
+
+func (c *WebreportController) check() {
+	userId, _ := c.GetSecureCookie(constant.CookieSecretKey, "user_id")
+	us := "2014@xiaochuankeji.cn"
 	n := DataSt{}
 	if err := c.ParseForm(&n); err != nil { //传入user指针
 		c.Ctx.WriteString("出错了！")
 	}
-	if len(n.Describe) > 0 && len(n.Name) > 0 {
-		// 插入数据操作
-		insert(n.Name, n.Describe, n.Xmyl, n.Jszb, n.Fx, n.Sm, n.Zb, n.Recipient)
-		log.Println("插入成功")
-		//插入成功后把\n换成<br>方便前端发送邮件解析
-		n.Describe = strings.Replace(n.Describe, "\n", "</br>", -1)
-		n.Xmyl = strings.Replace(n.Xmyl, "\n", "</br>", -1)
-		n.Jszb = strings.Replace(n.Jszb, "\n", "</br>", -1)
-		n.Fx = strings.Replace(n.Fx, "\n", "</br>", -1)
-		n.Zb = strings.Replace(n.Zb, "\n", "</br>", -1)
-		n.Sm = strings.Replace(n.Sm, "\n", "</br>", -1)
-		n.Recipient = strings.Replace(n.Recipient, "\n", "</br>", -1)
-		// 发送邮件
-		SendEmail(n)
-		//c.Ctx.WriteString(n.Name + ",插入成功！")
-		c.SuccessJson(nil)
-	} else {
-		log.Println("插入失败")
+	parts := strings.Split(n.Recipient, ";")
+	fmt.Println(parts)
+	userId = userId + us
+	result := in(userId, parts)
+	fmt.Println(result)
+	if result == true {
+		return
+	}
+	if result == false {
+		return
 	}
 
 }
 
-func SendEmail(n DataSt) {
+//添加测试报告
+func (c *WebreportController) Insert() {
+	c.check()
+	userId, _ := c.GetSecureCookie(constant.CookieSecretKey, "user_id")
+	us := "2014@xiaochuankeji.cn"
+	n := DataSt{}
+	if err := c.ParseForm(&n); err != nil { //传入user指针
+		c.Ctx.WriteString("出错了！")
+	}
+	parts := strings.Split(n.Recipient, ";")
+	fmt.Println(parts)
+	userId = userId + us
+	result := in(userId, parts)
+	fmt.Println(result)
+	if result == true {
+		if len(n.Describe) > 0 && len(n.Name) > 0 {
+			// 插入数据操作
+			insert(n.Name, n.Describe, n.Xmyl, n.Jszb, n.Fx, n.Sm, n.Zb, n.Recipient)
+			log.Println("插入成功")
+			//插入成功后把\n换成<br>方便前端发送邮件解析
+			n.Describe = strings.Replace(n.Describe, "\n", "</br>", -1)
+			n.Xmyl = strings.Replace(n.Xmyl, "\n", "</br>", -1)
+			n.Jszb = strings.Replace(n.Jszb, "\n", "</br>", -1)
+			n.Fx = strings.Replace(n.Fx, "\n", "</br>", -1)
+			n.Zb = strings.Replace(n.Zb, "\n", "</br>", -1)
+			n.Sm = strings.Replace(n.Sm, "\n", "</br>", -1)
+			n.Recipient = strings.Replace(n.Recipient, "\n", "</br>", -1)
+			// 发送邮件
+			SendEmail(n, userId)
+			fmt.Println(userId)
+			//c.Ctx.WriteString(n.Name + ",插入成功！")
+			c.SuccessJson(nil)
+		} else {
+			log.Println("插入失败")
+		}
+
+	}
+	if result == false {
+		fmt.Println("收件人无自己~")
+		n.Recipient = userId + ";" + n.Recipient
+		if len(n.Describe) > 0 && len(n.Name) > 0 {
+			// 插入数据操作
+			insert(n.Name, n.Describe, n.Xmyl, n.Jszb, n.Fx, n.Sm, n.Zb, n.Recipient)
+			log.Println("插入成功")
+			//插入成功后把\n换成<br>方便前端发送邮件解析
+			n.Describe = strings.Replace(n.Describe, "\n", "</br>", -1)
+			n.Xmyl = strings.Replace(n.Xmyl, "\n", "</br>", -1)
+			n.Jszb = strings.Replace(n.Jszb, "\n", "</br>", -1)
+			n.Fx = strings.Replace(n.Fx, "\n", "</br>", -1)
+			n.Zb = strings.Replace(n.Zb, "\n", "</br>", -1)
+			n.Sm = strings.Replace(n.Sm, "\n", "</br>", -1)
+			n.Recipient = strings.Replace(n.Recipient, "\n", "</br>", -1)
+			// 发送邮件
+			SendEmail(n, userId)
+			fmt.Println(userId)
+			//c.Ctx.WriteString(n.Name + ",插入成功！")
+			c.SuccessJson(nil)
+		} else {
+			log.Println("插入失败")
+		}
+	}
+}
+
+func SendEmail(n DataSt, userid string) {
+
 	user := "noapply2014@xiaochuankeji.cn"
 	password := "X6uQ4R1u"
 	host := "smtp.exmail.qq.com:25"
 	to := n.Recipient
+	//切分to，根据；遍历to是否有userid
+	//to.
+
 	//to := "fengmanlong2014@xiaochuankeji.cn;xueyibing2014@xiaochuankeji.cn;sunzhiying2014@xiaochuankeji.cn"
 
 	subject := "辛苦查收测试报告"
