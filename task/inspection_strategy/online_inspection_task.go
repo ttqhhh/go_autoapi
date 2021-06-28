4ab991847c1690371436ea6367333d7bed952f35
xueyibing <xueyibing2014@xiaochuankeji.cn>
2021-06-28 12:19:00 +0800

    发送巡检错误消息bug固定

--- task/inspection_strategy/online_inspection_task.go
+++ task/inspection_strategy/online_inspection_task.go
@@ -16,8 +16,10 @@ import (
 
 // 「测试效率团队」群web_hook-用来测试
 //const XIAO_NENG_QUN = "https://oapi.dingtalk.com/robot/send?access_token=6f35268d9dcb74b4b95dd338eb241832781aeaaeafd90aa947b86936f3343dbb"
+//const XIAO_NENG_QUN_TOKEN = "6f35268d9dcb74b4b95dd338eb241832781aeaaeafd90aa947b86936f3343dbb"
 // 「测试管理群」群web_hook-用来测试
-const XIAO_NENG_QUN = "https://oapi.dingtalk.com/robot/send?access_token=a822521c37e0d566563652452a1fdd692f27f1746d59c4229dd91047ba52f325"
+//const XIAO_NENG_QUN = "https://oapi.dingtalk.com/robot/send?access_token=a822521c37e0d566563652452a1fdd692f27f1746d59c4229dd91047ba52f325"
+const XIAO_NENG_QUN_TOKEN = "a822521c37e0d566563652452a1fdd692f27f1746d59c4229dd91047ba52f325"
 
 // todo 临时关闭状态，上线or正常使用时，需要设为true进行开启
 const IS_OPEN_SENDDING_MSG = false
@@ -153,8 +155,10 @@ func PerformInspection(businessId int8, serviceId int64, msgChannel chan string,
 				//break
 			}
 		}
-		totalMsg := baseMsg + msg
-		msgChannel <- totalMsg
+		if msg != "" {
+			totalMsg := baseMsg + msg
+			msgChannel <- totalMsg
+		}
 
 		// 更新失败个数和本次执行记录状态
 		autoResultMongo := &models.AutoResult{}
@@ -173,7 +177,7 @@ type ReqBody struct {
 }
 
 func DingSend(content string) {
-	var dingToken = []string{"6f35268d9dcb74b4b95dd338eb241832781aeaaeafd90aa947b86936f3343dbb"}
+	var dingToken = []string{XIAO_NENG_QUN_TOKEN}
 	cli := dingtalk.InitDingTalk(dingToken, "")
 	cli.SendTextMessage(content)
 }
