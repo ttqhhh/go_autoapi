28c80112ae3a3720769686b8df8432ca9006f284
xueyibing <xueyibing2014@xiaochuankeji.cn>
2021-06-29 15:09:30 +0800

    修改主键key值

--- models/flowreplay_mongo.go
+++ models/flowreplay_mongo.go
@@ -38,7 +38,7 @@ func (mongo *FlowReplayMongo) Insert(flowreplay FlowReplayMongo) error {
 
 	// id自增
 	r := utils.GetRedis()
-	id, err := r.Incr(constants.INSPECTION_CASE_PRIMARY_KEY).Result()
+	id, err := r.Incr(constants.Flow_Replay_PRIMARY_KEY).Result()
 	if err != nil {
 		logs.Error("保存Case时，获取从redis获取唯一主键报错，err: ", err)
 		return err
