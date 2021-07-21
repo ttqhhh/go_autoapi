207d641b7ef708771e9a9949a8e6068dbf79339b
xueyibing <xueyibing2014@xiaochuankeji.cn>
2021-07-21 20:08:42 +0800

    变更dev分支

--- wind_up.sh
+++ wind_up.sh
@@ -3,7 +3,7 @@
 function main() {
     develop_branch=$1
     # 检出dev分支
-    git checkout dev
+    git checkout develop
     # 将需求分支合并至dev分支, 并推至远程
     git merge  $develop_branch
     git push
@@ -21,11 +21,11 @@ function main() {
 }
 
 # 1、todo-done 首先确定本地已经检出了本地分支
-dev_branch=`git branch | grep "dev$"`
+dev_branch=`git branch | grep "develop$"`
 if [ ${dev_branch} ]; then
     echo "本地已存在dev分支, 后续直接执行checkout操作"
 else
-    git checkout -b dev origin/dev
+    git checkout -b develop origin/develop
 fi
 # 2、todo-done 其次确定本地已经检出了master分支
 master_branch=`git branch | grep "master$"`
