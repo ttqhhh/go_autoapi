5eee3e8c396caaecf28708cfb55209b04051678e
xueyibing <xueyibing2014@xiaochuankeji.cn>
2021-07-21 17:54:08 +0800

    合并代码脚本

--- wind_up.sh
+++ wind_up.sh
@@ -4,16 +4,20 @@ function main() {
     develop_branch=$1
     # 检出dev分支
     git checkout dev
-    # 将需求分支合并至dev分支
+    # 将需求分支合并至dev分支, 并推至远程
     git merge  $develop_branch
-    # 将合并后的dev分支打一个dev_tag
+    git push
+    # 将合并后的dev分支打一个dev_tag, 并推至远程
     git tag -a "dev_tag_${develop_branch}" -m "将分支${develop_branch}合并dev后，对dev打一个tag"
+    git push origin "dev_tag_${develop_branch}"
     # 检出master分支
     git checkout master
-    # 将合并需求分支后的dev分支合并至master分支
+    # 将合并需求分支后的dev分支合并至master分支, 并推至远程
     git merge dev
-    # 将合并后的master分支打一个master_tag
+    git push
+    # 将合并后的master分支打一个master_tag, 并推至远程
     git tag -a "master_tag_${develop_branch}" -m "将分支${develop_branch}合并master后，对master打一个tag"
+    git push origin "master_tag_${develop_branch}"
 }
 
 # 1、todo-done 首先确定本地已经检出了本地分支
