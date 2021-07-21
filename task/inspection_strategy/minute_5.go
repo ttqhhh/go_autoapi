93e403406e4299459d1e752c3e41fe94d84dd5f7
xueyibing <xueyibing2014@xiaochuankeji.cn>
2021-07-21 11:39:50 +0800

    删除tag验证代码

--- task/inspection_strategy/minute_5.go
+++ task/inspection_strategy/minute_5.go
@@ -1,47 +1,51 @@
 package inspection_strategy
 
-import "fmt"
+import (
+	"github.com/astaxie/beego/logs"
+	controllers "go_autoapi/controllers/autotest"
+	"go_autoapi/controllers/inspection"
+	"go_autoapi/models"
+)
 
 func Strategy5Min() error {
-	//logs.Info("【5分钟】级别的定时任务启动执行...")
-	////msgList := make([]string, 5)
-	//msgChannel := make(chan string)
-	//var msgList []string
-	//// 起一个协程用于承接msg
-	//go func() {
-	//	for true {
-	//		msg := <-msgChannel
-	//		msgList = append(msgList, msg)
-	//	}
-	//}()
-	//// 获取所以业务线，并进行遍历所有的业务线
-	//businesses := controllers.GetAllBusinesses()
-	//for _, business := range businesses {
-	//	// 遍历业务线下的所有所有服务
-	//	serviceMongo := models.ServiceMongo{}
-	//	businessId := int8(business["code"].(int))
-	//	serviceMongos, err := serviceMongo.QueryByBusiness(businessId)
-	//	if err != nil {
-	//		logs.Error("执行线上巡检定时任务时，查询指定业务线下的服务时报错， err: ", err)
-	//		return err
-	//	}
-	//	// 遍历服务下边所有的巡检Case
-	//	for _, service := range serviceMongos {
-	//		PerformInspection(businessId, service.Id, msgChannel, inspection.FIVE_MIN_CODE)
-	//	}
-	//
-	//}
-	//// dingMsg中的「线上巡检」为消息关键字，不可变更
-	//dingMsg := "小钻风【5分钟】级别线上巡检发现异样, 快去排查一下吧。\n"
-	//for _, msg := range msgList {
-	//	dingMsg += msg
-	//}
-	//if len(msgList) > 0 {
-	//	logs.Info("打印钉钉消息日志：\n" + dingMsg)
-	//	if IS_OPEN_SENDDING_MSG {
-	//		DingSend(dingMsg)
-	//	}
-	//}
-	fmt.Printf("打印一次日志验证tag部署")
+	logs.Info("【5分钟】级别的定时任务启动执行...")
+	//msgList := make([]string, 5)
+	msgChannel := make(chan string)
+	var msgList []string
+	// 起一个协程用于承接msg
+	go func() {
+		for true {
+			msg := <-msgChannel
+			msgList = append(msgList, msg)
+		}
+	}()
+	// 获取所以业务线，并进行遍历所有的业务线
+	businesses := controllers.GetAllBusinesses()
+	for _, business := range businesses {
+		// 遍历业务线下的所有所有服务
+		serviceMongo := models.ServiceMongo{}
+		businessId := int8(business["code"].(int))
+		serviceMongos, err := serviceMongo.QueryByBusiness(businessId)
+		if err != nil {
+			logs.Error("执行线上巡检定时任务时，查询指定业务线下的服务时报错， err: ", err)
+			return err
+		}
+		// 遍历服务下边所有的巡检Case
+		for _, service := range serviceMongos {
+			PerformInspection(businessId, service.Id, msgChannel, inspection.FIVE_MIN_CODE)
+		}
+
+	}
+	// dingMsg中的「线上巡检」为消息关键字，不可变更
+	dingMsg := "小钻风【5分钟】级别线上巡检发现异样, 快去排查一下吧。\n"
+	for _, msg := range msgList {
+		dingMsg += msg
+	}
+	if len(msgList) > 0 {
+		logs.Info("打印钉钉消息日志：\n" + dingMsg)
+		if IS_OPEN_SENDDING_MSG {
+			DingSend(dingMsg)
+		}
+	}
 	return nil
 }
--- task/task.go
+++ task/task.go
@@ -8,7 +8,7 @@ import (
 )
 
 // 是否开启巡检Case线上巡检任务(测试环境关闭)
-const IS_OPEN_INSPECTION_TASK = true
+const IS_OPEN_INSPECTION_TASK = false
 
 // 是否开启接口性能监控线上巡检任务(测试环境关闭)
 const IS_OPEN_RT_MONITOR_TASK = false
@@ -26,20 +26,20 @@ func init() {
 	if IS_OPEN_INSPECTION_TASK {
 		Inspection5M := toolbox.NewTask("Inspection_Task_5min", inspection.FIVE_MIN_EXPRESSION, inspection_strategy.Strategy5Min)
 		toolbox.AddTask("Inspection_Task_5min", Inspection5M)
-		//Inspection10M := toolbox.NewTask("Inspection_Task_10min", inspection.TEN_MIN_EXPRESSION, inspection_strategy.Strategy10Min)
-		//toolbox.AddTask("Inspection_Task_10min", Inspection10M)
-		//Inspection1Q := toolbox.NewTask("Inspection_Task_15min", inspection.ONE_QUARTER_EXPRESSION, inspection_strategy.Strategy15Min)
-		//toolbox.AddTask("Inspection_Task_15min", Inspection1Q)
-		//InspectionHalfH := toolbox.NewTask("Inspection_Task_30min", inspection.HALF_HOUR_EXPRESSION, inspection_strategy.Strategy30Min)
-		//toolbox.AddTask("Inspection_Task_30min", InspectionHalfH)
-		//Inspection1H := toolbox.NewTask("Inspection_Task_1hour", inspection.ONE_HOUR_EXPRESSION, inspection_strategy.Strategy1Hour)
-		//toolbox.AddTask("Inspection_Task_1hour", Inspection1H)
-		//Inspection6H := toolbox.NewTask("Inspection_Task_6hour", inspection.SIX_HOUR_EXPRESSION, inspection_strategy.Strategy6Hour)
-		//toolbox.AddTask("Inspection_Task_6hour", Inspection6H)
-		//InspectionHalfD := toolbox.NewTask("Inspection_Task_12hour", inspection.HALF_DAY_EXPRESSION, inspection_strategy.Strategy12Hour)
-		//toolbox.AddTask("Inspection_Task_12hour", InspectionHalfD)
-		//Inspection1D := toolbox.NewTask("Inspection_Task_1day", inspection.ONE_DAY_EXPRESSION, inspection_strategy.Strategy1Day)
-		//toolbox.AddTask("Inspection_Task_1day", Inspection1D)
+		Inspection10M := toolbox.NewTask("Inspection_Task_10min", inspection.TEN_MIN_EXPRESSION, inspection_strategy.Strategy10Min)
+		toolbox.AddTask("Inspection_Task_10min", Inspection10M)
+		Inspection1Q := toolbox.NewTask("Inspection_Task_15min", inspection.ONE_QUARTER_EXPRESSION, inspection_strategy.Strategy15Min)
+		toolbox.AddTask("Inspection_Task_15min", Inspection1Q)
+		InspectionHalfH := toolbox.NewTask("Inspection_Task_30min", inspection.HALF_HOUR_EXPRESSION, inspection_strategy.Strategy30Min)
+		toolbox.AddTask("Inspection_Task_30min", InspectionHalfH)
+		Inspection1H := toolbox.NewTask("Inspection_Task_1hour", inspection.ONE_HOUR_EXPRESSION, inspection_strategy.Strategy1Hour)
+		toolbox.AddTask("Inspection_Task_1hour", Inspection1H)
+		Inspection6H := toolbox.NewTask("Inspection_Task_6hour", inspection.SIX_HOUR_EXPRESSION, inspection_strategy.Strategy6Hour)
+		toolbox.AddTask("Inspection_Task_6hour", Inspection6H)
+		InspectionHalfD := toolbox.NewTask("Inspection_Task_12hour", inspection.HALF_DAY_EXPRESSION, inspection_strategy.Strategy12Hour)
+		toolbox.AddTask("Inspection_Task_12hour", InspectionHalfD)
+		Inspection1D := toolbox.NewTask("Inspection_Task_1day", inspection.ONE_DAY_EXPRESSION, inspection_strategy.Strategy1Day)
+		toolbox.AddTask("Inspection_Task_1day", Inspection1D)
 	}
 	if IS_OPEN_RT_MONITOR_TASK {
 		RtMonitorTask := toolbox.NewTask("Rt_Monitor_Task", monitor.MONITOR_TASK_EXPRESSION, monitor.MonitorTask)
