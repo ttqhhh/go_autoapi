d57c7668f0a845bc50c91d2c69f90c9c4c22a988
tangtianqing <tangtianqing2014@xiaochuankeji.cn>
2021-08-04 14:57:13 +0800

    修复删除报告没有删除报告结果，导致查询效率没变的问题

--- task/inspection_strategy/week_1.go
+++ task/inspection_strategy/week_1.go
@@ -12,7 +12,11 @@ import (
 
 func Delete1Week() error {
 	logs.Info("【周】定期批量清理报告任务启动执行。。。。。。")
-	var once = 0  //计数器
+	var once = 0  //报告计数器
+	var once1 =0  //报告结果计数器
+	/**
+	删除报告库
+	 */
 	var rp = models.RunReportMongo{}
 	runReportList, err := rp.Query()
 	if nil !=err{
@@ -28,12 +32,34 @@ func Delete1Week() error {
 			reportCreateTime,_ := time.ParseInLocation(Layout,reportCreateStringTime,time.Local) //转换为date类型
 
 			if reportCreateTime.Before(time.Now().AddDate(0,0,-7)){ //如果报告时间超过一周
-
 				rp.Delete(report.Id)
 				once=once+1  //执行一次，次数+1
 			}
 		}
 		times := strconv.Itoa(once)
 		logs.Info("批量删除成功,共删除"+times+"条数据")
+	/**
+	删除结果库(线上库中报告中和报告及结果对应的报告已经删除，所以通过时间删除)
+	*/
+		var rpResult = models.AutoResult{}
+		autoResultList, err := rpResult.QueryResult()
+			count1:=len(autoResultList)
+			str2 := strconv.Itoa(count1)
+			fmt.Printf("查询到报告的结果为"+str2+"，开始删除超过一周的报告结果\n\n")
+
+	if err!= nil{
+			logs.Info("查询错误err:",err)
+		}
+		for _,result:= range autoResultList{
+			resultCreateStringTime :=result.CreatedAt
+			const Layout = "2006-01-02 15:04:05"//时间常量
+			resultCreateTime ,_ := time.ParseInLocation(Layout,resultCreateStringTime,time.Local) //转换为date类型
+			if resultCreateTime.Before(time.Now().AddDate(0,0,-8)){ //如果报告时间超过一周（为了防止报告结果查不到，留下多一天的报告结果）
+				rpResult.DeleteResult(result.RunId)
+			once1++
+			}
+		}
+		timess := strconv.Itoa(once1)
+		logs.Info("批量删除成功,共删除"+timess+"条报告结果")
 		return nil
 }
