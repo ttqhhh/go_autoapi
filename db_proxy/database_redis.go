25f00e89d156e8396084811befdf2453e8634eac
wangzhen01 <654867391>
2021-08-23 15:13:56 +0800

    fix conf to test

--- db_proxy/database_redis.go
+++ db_proxy/database_redis.go
@@ -10,8 +10,8 @@ var rdb *redis.Client
 // 初始化连接
 func InitClient() (err error) {
 	rdb = redis.NewClient(&redis.Options{
-		//Addr: "172.20.20.2:6379", // 测试
-		Addr:     "172.16.2.86:6379", // 生产
+		Addr: "172.20.20.2:6379", // 测试
+		//Addr:     "172.16.2.86:6379", // 生产
 		Password: "",                 // no password set
 		DB:       0,                  // use default DB
 	})
