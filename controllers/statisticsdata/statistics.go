130ec7ea8c3515e774aa70467208502fdfa16a8b
tangtianqing <tangtianqing2014@xiaochuankeji.cn>
2022-03-09 20:43:46 +0800

    统计商业化功能实现，明天增加全业务线

--- controllers/statisticsdata/statistics.go
+++ controllers/statisticsdata/statistics.go
@@ -1,9 +1,12 @@
 package statisticsdata
 
 import (
+	"encoding/json"
+	"github.com/astaxie/beego/httplib"
 	"github.com/astaxie/beego/logs"
 	"go_autoapi/libs"
 	"go_autoapi/models"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
@@ -442,6 +445,68 @@ func getAllApi() map[string]float64 {
 	//data["5"] = 200
 	//data["6"] = 200
 
+	cookie := getLogin()
+	//for i := range list{  //遍历所有getway
+	//
+	//}
+	respData := doReq(AD_gateway_path_url, cookie)
+	toJson := JsonData{}
+	err := json.Unmarshal([]byte(respData), &toJson)
+	if err != nil {
+
+	}
 	return data
 
+}
+
+//-----------------------------------------------------------------
+
+//用到的常量
+const ZY_grafana_login_url = "http://grafana.ixiaochuan.cn/login"
+const AD_gateway_path_url = "http://grafana.ixiaochuan.cn/api/datasources/proxy/1/api/v1/query_range?query=sum(rate(xms_gateway_ad_http_latency_count%5B1m%5D))by(uri)&start=1646064000&end=1648742400&step=7200"
+
+//解析json所需要的结构体
+type JsonData struct {
+	Status string `json:"status"`
+	Data   Data   `json:"data"`
+}
+
+type Data struct {
+	ResultType string   `json:"resultType"`
+	Result     []Result `json:"result"`
+}
+
+type Result struct {
+	Meturc Metruc          `json:"metric"`
+	Values [][]interface{} `json:"values"`
+}
+
+type Metruc struct {
+	Uri string `json:"uri"`
+}
+
+//获取外部接口的所有方法
+func getLogin() *http.Cookie {
+	req := httplib.Post(ZY_grafana_login_url)
+	req.Param("user", "wangzhen01")
+	req.Param("password", "Iepohg5go4iawoo")
+	req.Param("email", "")
+	resp, err := req.Response()
+	if err != nil {
+	}
+	cookies := resp.Cookies()
+	for _, cookie := range cookies {
+		return cookie
+	}
+	return nil
+}
+
+func doReq(url string, cookie *http.Cookie) string {
+	req := httplib.Get(url)
+	req.SetCookie(cookie)
+	str, err := req.String()
+	if err != nil {
+		//
+	}
+	return str
 }
--- tests/test.go
+++ tests/test.go
@@ -1,18 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
+	"github.com/astaxie/beego/httplib"
+	"net/http"
 )
 
-func test1 () {
-	if strings.Contains("线上回归测试", "sb") {
-		fmt.Print("ok")
-	}
-	fmt.Println("fin")
+const ZY_grafana_login_url = "http://grafana.ixiaochuan.cn/login"
+const AD_gateway_path_url = "http://grafana.ixiaochuan.cn/api/datasources/proxy/1/api/v1/query_range?query=sum(rate(xms_gateway_ad_http_latency_count%5B1m%5D))by(uri)&start=1646064000&end=1648742400&step=7200"
 
+type JsonData struct {
+	Status string `json:"status"`
+	Data   Data   `json:"data"`
 }
 
-func main (){
-	test1()
+type Data struct {
+	ResultType string   `json:"resultType"`
+	Result     []Result `json:"result"`
+}
+
+type Result struct {
+	Meturc Metruc          `json:"metric"`
+	Values [][]interface{} `json:"values"`
+}
+
+type Metruc struct {
+	Uri string `json:"uri"`
+}
+
+//type Values struct {
+//	Values [][]int`json:"values"`
+//}
+
+func getLogin() *http.Cookie {
+	req := httplib.Post(ZY_grafana_login_url)
+	req.Param("user", "wangzhen01")
+	req.Param("password", "Iepohg5go4iawoo")
+	req.Param("email", "")
+	resp, err := req.Response()
+	if err != nil {
+	}
+	cookies := resp.Cookies()
+	for _, cookie := range cookies {
+		return cookie
+	}
+	return nil
+}
+
+func doReq(url string, cookie *http.Cookie) string {
+	req := httplib.Get(url)
+	req.SetCookie(cookie)
+	str, err := req.String()
+	if err != nil {
+		//
+	}
+	return str
+}
+
+func main() {
+	cookie := getLogin()
+	//for i := range list{  //遍历所有getway
+	//
+	//}
+	respData := doReq(AD_gateway_path_url, cookie)
+	toJson := JsonData{}
+	err := json.Unmarshal([]byte(respData), &toJson)
+	if err != nil {
+
+	}
+	count := 0
+	for _, one := range toJson.Data.Result {
+		for _, ones := range one.Values {
+			if ones[1] != "0" {
+				count++
+				break
+			}
+		}
+	}
+	fmt.Print("活跃接口数为：")
+	fmt.Print(count)
+	fmt.Print("\n")
+
 }
